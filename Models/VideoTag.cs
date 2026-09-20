7ae007352921cff1e6c35336ca40151970024d89
andrew.t.shilt.civ <andrew.t.shilt.civ@health.mil>
2026-09-20 10:45:48 -0400

    Fixed acocunt controller

--- Models/VideoTag.cs
+++ Models/VideoTag.cs
@@ -3,9 +3,9 @@ namespace VideoTube.Models
     public class VideoTag
     {
         public int VideoId { get; set; }
-        public Video? Video { get; set; }
+        public Video Video { get; set; }
 
         public int TagId { get; set; }
-        public Tag? Tag { get; set; }
+        public Tag Tag { get; set; }
     }
 }
