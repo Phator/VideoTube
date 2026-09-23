f85f993dbe66e8d208f4e6142eab2b1f2d16c852
andrew.t.shilt.civ <andrew.t.shilt.civ@health.mil>
2026-09-23 16:54:10 -0400

    Added New Badge

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.Features;
 using VideoTube.Data;
 using VideoTube.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+// MVC
 builder.Services.AddControllersWithViews();
 
+// Database
 builder.Services.AddDbContext<ApplicationDbContext>(
     options =>
         options.UseSqlServer(
-            builder.Configuration.GetConnectionString(
-                "DefaultConnection")));
+            builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Identity
 builder.Services
     .AddDefaultIdentity<ApplicationUser>(options =>
     {
@@ -29,22 +30,25 @@ builder.Services
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+// Cookie paths
 builder.Services.ConfigureApplicationCookie(options =>
-    {
+{
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/Login";
-    });
+});
 
+// Logger
 builder.Services.AddScoped<ActivityLogger>();
 
+// Upload size
 builder.Services.Configure<FormOptions>(options =>
 {
-    options.MultipartBodyLengthLimit =
-        5L * 1024L * 1024L * 1024L; // 5 GB
+    options.MultipartBodyLengthLimit = 5L * 1024L * 1024L * 1024L; // 5 GB
 });
 
 var app = builder.Build();
 
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -52,15 +56,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
+// Routing
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Video}/{action=Index}/{id?}");
